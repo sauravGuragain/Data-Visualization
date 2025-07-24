be1e341213057f74ffd4bf4d55a1a70fbb0a755a
Saurav Guragain <grgsaurav51@gmail.com>
2025-07-24 15:08:32 +0545

    Time series forcasting using statsmodel api

---
+++ Time Series Forcasting.py
@@ -0,0 +1,11 @@
+"""Loading the AirPassengers Dataset from Stats Model for Time Series forcasting"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+data = sm.datasets.get_rdataset("AirPassengers", "datasets").data
+data.index = pd.date_range(start="1949-01", periods=len(data), freq="ME")
+ts = data["value"] 
+ts.plot(title="Monthly Air Passengers (1949–1960)")
+plt.ylabel("Passengers (1000s)")
+plt.show()
