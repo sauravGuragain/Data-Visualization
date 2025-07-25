d8a30a6949771d714860c341383af084c59bcbc4
Saurav Guragain <grgsaurav51@gmail.com>
2025-07-25 09:43:13 +0545

    Using VAR to see how other time series influence data

--- Time Series Forcasting.py
+++ Time Series Forcasting.py
@@ -47,3 +47,41 @@ mse = mean_squared_error(test, predictions)
 rmse = np.sqrt(mse)
 print("RMSE:", rmse)
 
+# %%
+"""Using VAR to see what and how other influence series"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.api import VAR
+np.random.seed(42)
+
+dates = pd.date_range(start='2015-01', periods=100, freq='ME')
+ads = np.random.normal(loc=2000, scale=300, size=100)  
+sales = 50 + 0.5 * ads + np.random.normal(scale=100, size=100)  
+
+df = pd.DataFrame({'Ads': ads, 'Sales': sales}, index=dates)
+
+df.plot(title="Sales vs Ads over Time", figsize=(10,5))
+plt.show()
+
+df_diff = df.diff().dropna()
+
+df_diff.plot(title="Differenced Sales and Ads", figsize=(10,5))
+plt.show()
+
+model = VAR(df_diff)
+lag_selection = model.select_order(maxlags=15)
+print(lag_selection.summary())
+fitted_model = model.fit(lag_selection.aic)
+forecast_input = df_diff.values[-lag_selection.aic:]
+forecast = fitted_model.forecast(y=forecast_input, steps=5)
+
+forecast_df = pd.DataFrame(forecast, columns=['Ads_forecast', 'Sales_forecast'])
+print(forecast_df)
+
+last_known = df.iloc[-1]
+forecast_original = forecast_df.cumsum() + last_known
+print(forecast_original)
+# %%
+
+
